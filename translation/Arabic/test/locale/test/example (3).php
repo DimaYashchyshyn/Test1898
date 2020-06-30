821e4d4a962356d7a020e85a28ad500f4f53e04f
Dmytro228 <67628727+Dmytro228@users.noreply.github.com>
2020-06-30 12:29:17 +0300

    New translations example (3).php (Arabic)

---
+++ translation/Arabic/test/locale/test/example (3).php
@@ -0,0 +1,12 @@
+﻿<?php
+$en_lang = array (
+"Identifier_1"=>'سلسلة للترجمة 1',
+"Identifier_2"=>'نص الترجمة',
+"Identifier_3"=>'سلسلة للترجمة 3',
+"Identifier_4"=>'سلسلة للترجمة 4',
+"Identifier_6"=>'Тест',
+"Identifier_7"=>'Тест',
+"Identifier_5"=>'سلسلة للترجمة 5',
+);
+extract($en_lang);
+?>
