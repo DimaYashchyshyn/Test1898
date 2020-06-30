0da7a17ed0e554dfe18b550ad509315143306ea6
Dmytro228 <67628727+Dmytro228@users.noreply.github.com>
2020-06-30 12:29:06 +0300

    New translations example.php (Polish)

---
+++ translation/Polish/test/locale/test/example.php
@@ -0,0 +1,12 @@
+﻿<?php
+$en_lang = array (
+"Identifier_1"=>'Ciąg dla tłumaczenia 1',
+"Identifier_2"=>'Ciąg dla tłumaczenia 2',
+"Identifier_3"=>'Ciąg dla tłumaczenia 3',
+"Identifier_4"=>'Ciąg dla tłumaczenia 4',
+"Identifier_6"=>'Тест',
+"Identifier_7"=>'Тест',
+"Identifier_5"=>'Ciąg dla tłumaczenia 5',
+);
+extract($en_lang);
+?>
