a0dd264e865a87aa982dc73b6d888f0d0b72b37c
Dima Yashchyshyn <67628727+DimaYashchyshyn@users.noreply.github.com>
2021-04-09 03:06:23 +0300

    New translations example (1).php (Arabic)

---
+++ translation/Arabic/test/locale/test/example (1).php
@@ -0,0 +1,12 @@
+﻿<?php
+$en_lang = array (
+"Identifier_1"=>'String for translation 1',
+"Identifier_2"=>'String for translation 2',
+"Identifier_3"=>'String for translation 3',
+"Identifier_4"=>'String for translation 4',
+"Identifier_6"=>'Тест',
+"Identifier_7"=>'Тест',
+"Identifier_5"=>'String for translation 5',
+);
+extract($en_lang);
+?>
